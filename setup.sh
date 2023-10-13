944276f35b36d8ef7a2ef474669c1200711d9497
hibiki-kato <katohibiki17@gmail.com>
2023-10-13 23:01:02 +0900

    change pyenv instl in .sh

--- setup.sh
+++ setup.sh
@@ -4,22 +4,28 @@
 sudo apt-get update
 
 # Install git, tree, vim, pigpio, and Python dependencies
-sudo apt install -y build-essential git tree vim pigpio make build-essential libssl-dev zlib1g-dev \
+sudo apt-get install -y build-essential git tree vim pigpio make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 sudo service pigpiod start # Start pigpio daemon
 sudo systemctl enable pigpiod.service # Enable pigpio daemon on boot
 
-# Add pyenv to PATH
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-
-# Install pyenv
-curl https://pyenv.run | bash
-
+if [ ! -r ~/.pyenv/ ]
+then
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    echo '
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)" # Enable auto-activation of virtualenvs
+fi' >> ~/.bashrc
+    source ~/.bashrc
+    exec "$SHELL"
+fi
 
 
 
@@ -30,12 +36,12 @@ pyenv global 3.7.3
 
 echo 1 | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.
 
-# # Install Poetry
-# curl -sSL https://install.python-poetry.org | python3 -
+# Install Poetry
+curl -sSL https://install.python-poetry.org | python3 -
 
-# # Add Poetry to PATH
-# echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
-# source ~/.bashrc
+# Add Poetry to PATH
+echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 
 
 
