b2c9497bce964474f207b9bb0ad9784c4ee0016e
hibiki-kato <katohibiki17@gmail.com>
2023-10-20 09:42:02 +0000

    fix bug

--- ikkyo-sai_2023/ikkyo_sai_2023/main.py
+++ ikkyo-sai_2023/ikkyo_sai_2023/main.py
@@ -4,11 +4,11 @@ import MoterClass as mc
 from typing import List, Tuple
 import asyncio
 
-async def play_async(motor: mc.Motor, state_label: tk.Label, stop_label: tk.Label) -> None:
+async def play_async(motor: mc.Motor, state_label: tk.Label) -> None:
     motor.play()
     state_label.config(text="再生中", bg="green")
 
-async def stop_async(motor: mc.Motor, state_label: tk.Label, stop_label: tk.Label) -> None:
+async def stop_async(motor: mc.Motor, state_label: tk.Label) -> None:
     motor.stop()
     state_label.config(text="停止中", bg="red")
 
@@ -25,6 +25,7 @@ async def main_async():
     # GUIの作成
     root: tk.Tk = tk.Tk()
     root.title("一教祭2023")
+    root.geometry("1000x1000")
 
     # ボタンとラベルの作成と配置
     buttons: List[Tuple[tk.Button, tk.Button, tk.Label]] = []
@@ -32,13 +33,13 @@ async def main_async():
         frame: tk.Frame = tk.Frame(root)
         frame.pack(side=tk.LEFT, padx=10)
         
-        play_button: tk.Button = tk.Button(frame, text="再生", height=10, width=10, command=lambda m=motor: asyncio.create_task(play_async(m, state_label, stop_label)), bg="green", font=("", 20))
+        play_button: tk.Button = tk.Button(frame, text="再生", height=10, width=20, command=lambda m=motor: asyncio.create_task(play_async(m, state_label, stop_label)), bg="green", font=("", 20))
         play_button.pack()
 
-        stop_button: tk.Button = tk.Button(frame, text="停止",height=10, width=10, command=lambda m=motor: asyncio.create_task(stop_async(m, state_label, stop_label)), bg="red", font=("", 20))
+        stop_button: tk.Button = tk.Button(frame, text="停止",height=10, width=20, command=lambda m=motor: asyncio.create_task(stop_async(m, state_label, stop_label)), bg="red", font=("", 20))
         stop_button.pack()
 
-        state_label: tk.Label = tk.Label(frame, height=10, width=10, text="停止中", bg="red", font=("", 20))
+        state_label: tk.Label = tk.Label(frame, height=10, width=20, text="停止中", bg="red", font=("", 20))
         state_label.pack()
 
         buttons.append((play_button, stop_button, state_label))
