cecccf4df5706780c8e6aae7cf636004d4bbb36e
hibiki-kato <katohibiki17@gmail.com>
2023-10-20 07:35:14 +0000

    modified GUI

--- ikkyo-sai_2023/ikkyo_sai_2023/main.py
+++ ikkyo-sai_2023/ikkyo_sai_2023/main.py
@@ -1,147 +1,23 @@
-# BEGIN: 8f7d2j3d9d2
-import tkinter as tk
-import tkinter.ttk as ttk
-from typing import List
-import RPi.GPIO as GPIO
 import time
-import csv
+import tkinter as tk
 
-class Motor:
-    def __init__(self, pin: int, filepath: str) -> None:
-        """
-        Initialize the Motor class.
 
-        Args:
-        - pin (int): The GPIO pin number to which the motor is connected.
-        - filepath (str): The path to the CSV file containing the motor instructions.
-        """
-        self.pin = pin
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pin, 1000)
-        self.pwm.start(0) # 0% duty cycle
-        
-        self.instructions = self.read_motor_instructions(filepath)
+# GUIの作成
+root: tk.Tk = tk.Tk()
 
-    def set_duty_cycle(self, duty_cycle: float) -> None:
-        """
-        Set the duty cycle of the PWM signal.
+# ボタンの作成と配置
+buttons: List[Tuple[tk.Button, tk.Button, tk.Button]] = []
+for i in range(4):
+    frame: tk.Frame = tk.Frame(root)
+    frame.pack(side=tk.LEFT, padx=10)
 
-        Args:
-        - duty_cycle (float): The duty cycle of the PWM signal.
-        """
-        self.pwm.ChangeDutyCycle(duty_cycle)
+    play_button: tk.Button = tk.Button(frame, text="再生")
+    play_button.pack()
 
-    def stop(self) -> None:
-        """
-        Stop the motor.
-        """
-        self.pwm.stop()
+    pause_button: tk.Button = tk.Button(frame, text="一時停止")
+    pause_button.pack()
 
-    def cleanup(self) -> None:
-        """
-        Clean up the GPIO pins.
-        """
-        GPIO.cleanup()
+    reset_button: tk.Button = tk.Button(frame, text="リセット")
+    reset_button.pack()
 
-    def drive_for_time(self, n: float, v: float) -> None:
-        """
-        Drive the motor for a specified amount of time.
-
-        Args:
-        - n (float): The duration for which the motor should be driven.
-        - v (float): The duty cycle of the PWM signal.
-        """
-        self.set_duty_cycle(v)
-        time.sleep(n)
-        self.stop()
-
-    def read_motor_instructions(self, filepath: str) -> List[List[float]]:
-        """
-        Read motor instructions from a CSV file.
-
-        Args:
-        - filepath (str): The path to the CSV file.
-
-        Returns:
-        - A list of lists containing the motor instructions.
-        """
-        with open(filepath, 'r') as f:
-            reader = csv.reader(f)
-            instructions = []
-            for row in reader:
-                instructions.append([float(x) for x in row])
-        return instructions
-
-    def run_motor(self) -> None:
-        """
-        Run the motor according to the instructions.
-
-        Args:
-        - instructions (List[List[float]]): A list of lists containing the motor instructions.
-        """
-        start_time = time.time()
-        for instruction in self.instructions:
-            timestamp = instruction[0]
-            duration = instruction[1]
-            duty_cycle = instruction[2]
-            while time.time() - start_time < timestamp:
-                pass
-            self.drive_for_time(duration, duty_cycle)
-
-class App:
-    def __init__(self, master):
-        self.master = master
-        self.motors = []
-        self.create_widgets()
-
-    def create_widgets(self):
-        # Create buttons
-        self.play_button = tk.Button(self.master, text="再生", command=self.play)
-        self.stop_button = tk.Button(self.master, text="停止", command=self.stop)
-
-        # Create progress bars
-        self.progress_bars = []
-        for i in range(4):
-            progress_bar = ttk.Progressbar(self.master, orient="horizontal", length=200, mode="determinate")
-            progress_bar.grid(row=i, column=1)
-            self.progress_bars.append(progress_bar)
-
-    def play(self):
-        # Create motor instances
-        self.motors = [Motor(1, "motor1.csv"), Motor(2, "motor2.csv"), Motor(3, "motor3.csv"), Motor(4, "motor4.csv")]
-
-        # Get total duration
-        total_duration = max([motor.instructions[-1][0] + motor.instructions[-1][1] for motor in self.motors])
-
-        # Update progress bars
-        for i in range(4):
-            self.progress_bars[i]["maximum"] = total_duration
-            self.progress_bars[i]["value"] = 0
-
-        # Run motors
-        start_time = time.time()
-        while time.time() - start_time < total_duration:
-            for i, motor in enumerate(self.motors):
-                if time.time() - start_time >= motor.instructions[-1][0] + motor.instructions[-1][1]:
-                    self.progress_bars[i]["value"] = self.progress_bars[i]["maximum"]
-                else:
-                    timestamp = time.time() - start_time
-                    for instruction in motor.instructions:
-                        if instruction[0] <= timestamp < instruction[0] + instruction[1]:
-                            duty_cycle = instruction[2]
-                            motor.set_duty_cycle(duty_cycle)
-                            self.progress_bars[i]["value"] = timestamp
-                            break
-
-    def stop(self):
-        for motor in self.motors:
-            motor.stop()
-        for progress_bar in self.progress_bars:
-            progress_bar.stop()
-
-root = tk.Tk()
-app = App(root)
-root.mainloop()
-# END: 8f7d2j3d9d2
-
+    buttons.append((play_button, pause_button, reset_button))
