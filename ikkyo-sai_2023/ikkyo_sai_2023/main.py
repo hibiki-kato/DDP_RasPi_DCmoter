604fa9a40062ecd2fbb72c27b6f6b69c9c055477
hibiki-kato <katohibiki17@gmail.com>
2023-10-20 09:10:56 +0000

    add display

--- ikkyo-sai_2023/ikkyo_sai_2023/main.py
+++ ikkyo-sai_2023/ikkyo_sai_2023/main.py
@@ -3,33 +3,56 @@ import tkinter as tk
 import MoterClass as mc
 from typing import List, Tuple
 
-#pinの設定
-pins: List[int] = [17, 18, 27, 22]
+def main():
+    #pinの設定
+    pins: List[int] = [17, 18, 27, 22]
 
-#モータークラスのインスタンス化
-moters: List[mc.Motor] = []
-for i in range(4):
-    moters.append(mc.Motor(pins[i], "instruction.csv"))
+    #モータークラスのインスタンス化
+    moters: List[mc.Motor] = []
+    for i in range(4):
+        moters.append(mc.Motor(pins[i], "instruction.csv"))
 
 
-# GUIの作成
-root: tk.Tk = tk.Tk()
-root.geometry("1200x900")
-root.title("一教祭2023")
+    # GUIの作成
+    root: tk.Tk = tk.Tk()
+    root.geometry("1200x900")
+    root.title("一教祭2023")
 
-# ボタンの作成と配置
-buttons: List[Tuple[tk.Button, tk.Button]] = []
-for motor in moters:
-    frame: tk.Frame = tk.Frame(root)
-    frame.pack(side=tk.LEFT, padx=10)
+    # ボタンとラベルの作成と配置
+    buttons: List[Tuple[tk.Button, tk.Button, tk.Label]] = []
+    for motor in moters:
+        frame: tk.Frame = tk.Frame(root)
+        frame.pack(side=tk.LEFT, padx=10)
 
-    play_button: tk.Button = tk.Button(frame, text="再生", width=10, height=5, command = motor.play())
-    play_button.pack()
+        play_button: tk.Button = tk.Button(frame, text="再生", command=lambda m=motor: play(m, play_label, stop_label))
+        play_button.pack()
 
-    reset_button: tk.Button = tk.Button(frame, text="リセット", width=10, height=5, command = motor.stop())
-    reset_button.pack()
+        stop_button: tk.Button = tk.Button(frame, text="停止", command=lambda m=motor: stop(m, play_label, stop_label))
+        stop_button.pack()
 
-    buttons.append((play_button, reset_button))
+        play_label: tk.Label = tk.Label(frame, text="停止中")
+        play_label.pack()
+
+        stop_label: tk.Label = tk.Label(frame, text="")
+        stop_label.pack()
+
+        buttons.append((play_button, stop_button, play_label, stop_label))
         
-# BEGIN: 8zj5t3d7f2q1
-root.mainloop()
+    # メインループ
+    tk.mainloop()
+
+def play(motor: mc.Motor, play_label: tk.Label, stop_label: tk.Label) -> None:
+    motor.play()
+    motor.is_playing = True
+    play_label.config(text="再生中")
+    stop_label.config(text="")
+
+def stop(motor: mc.Motor, play_label: tk.Label, stop_label: tk.Label) -> None:
+    motor.stop()
+    motor.is_playing = False
+    play_label.config(text="停止中")
+    stop_label.config(text="停止中")
+    
+if __name__ == "__main__":
+    main()
+
