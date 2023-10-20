9e636e9098c46153d8d90f865ff25ce6030d9e5f
hibiki-kato <katohibiki17@gmail.com>
2023-10-20 10:13:04 +0000

    add all contrall button

--- ikkyo-sai_2023/ikkyo_sai_2023/main.py
+++ ikkyo-sai_2023/ikkyo_sai_2023/main.py
@@ -1,16 +1,7 @@
-import time
 import tkinter as tk
 import MoterClass as mc
 from typing import List, Tuple
 import asyncio
-
-async def play_async(motor: mc.Motor, state_label: tk.Label) -> None:
-    motor.play()
-    state_label.config(text="再生中", bg="green")
-
-async def stop_async(motor: mc.Motor, state_label: tk.Label) -> None:
-    motor.stop()
-    state_label.config(text="停止中", bg="red")
 
 async def main_async():
     #pinの設定
@@ -43,8 +34,24 @@ async def main_async():
         state_label.pack()
 
         buttons.append((play_button, stop_button, state_label))
-        
+    
+    #下に一斉再生と一斉停止のボタンを作成
+    frame: tk.Frame = tk.Frame(root)
+    frame.pack(side=tk.LEFT, padx=10)
+    play_all_button: tk.Button = tk.Button(frame, text="一斉再生", height=10, width=20, command=lambda: [asyncio.create_task(play_async(m, objects[2])) for (m, objects) in zip(moters, buttons)], bg="green", font=("", 20))
+    play_all_button.pack()
+    stop_all_button: tk.Button = tk.Button(frame, text="一斉停止", height=10, width=20, command=lambda: [asyncio.create_task(stop_async(m, objects[2])) for (m, objects) in zip(moters, buttons)], bg="red", font=("", 20))
+    stop_all_button.pack()
+    
     root.mainloop()
 
 if __name__ == "__main__":
     asyncio.run(main_async())
+
+async def play_async(motor: mc.Motor, state_label: tk.Label) -> None:
+    motor.play()
+    state_label.config(text="再生中", bg="green")
+
+async def stop_async(motor: mc.Motor, state_label: tk.Label) -> None:
+    motor.stop()
+    state_label.config(text="停止中", bg="red")
