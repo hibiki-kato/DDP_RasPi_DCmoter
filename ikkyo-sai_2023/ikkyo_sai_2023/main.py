f73d31e9af7a3cd222bff35e945f243cd591b0d0
hibiki-kato <katohibiki17@gmail.com>
2023-10-20 09:39:44 +0000

    fix bug

--- ikkyo-sai_2023/ikkyo_sai_2023/main.py
+++ ikkyo-sai_2023/ikkyo_sai_2023/main.py
@@ -2,8 +2,17 @@ import time
 import tkinter as tk
 import MoterClass as mc
 from typing import List, Tuple
+import asyncio
 
-def main():
+async def play_async(motor: mc.Motor, state_label: tk.Label, stop_label: tk.Label) -> None:
+    motor.play()
+    state_label.config(text="再生中", bg="green")
+
+async def stop_async(motor: mc.Motor, state_label: tk.Label, stop_label: tk.Label) -> None:
+    motor.stop()
+    state_label.config(text="停止中", bg="red")
+
+async def main_async():
     #pinの設定
     pins: List[int] = [17, 18, 27, 22]
 
@@ -23,29 +32,18 @@ def main():
         frame: tk.Frame = tk.Frame(root)
         frame.pack(side=tk.LEFT, padx=10)
         
-        play_button: tk.Button = tk.Button(frame, text="再生", height=10, width=10, command=lambda m=motor: play(m, play_label, stop_label), bg="green", font=("", 20))
+        play_button: tk.Button = tk.Button(frame, text="再生", height=10, width=10, command=lambda m=motor: asyncio.create_task(play_async(m, state_label, stop_label)), bg="green", font=("", 20))
         play_button.pack()
 
-        stop_button: tk.Button = tk.Button(frame, text="停止",height=10, width=15, command=lambda m=motor: stop(m, play_label, stop_label), bg="red", font=("", 20))
+        stop_button: tk.Button = tk.Button(frame, text="停止",height=10, width=15, command=lambda m=motor: asyncio.create_task(stop_async(m, state_label, stop_label)), bg="red", font=("", 20))
         stop_button.pack()
 
-        play_label: tk.Label = tk.Label(frame, height=10, width=10, text="停止中", bg="red", font=("", 20))
-        play_label.pack()
+        state_label: tk.Label = tk.Label(frame, height=10, width=10, text="停止中", bg="red", font=("", 20))
+        state_label.pack()
 
-        stop_label: tk.Label = tk.Label(frame, height=10, width=10, text="停止中", bg="red", font=("", 20))
-        stop_label.pack()
-
-        buttons.append((play_button, stop_button, play_label))
+        buttons.append((play_button, stop_button, state_label))
         
     root.mainloop()
 
-def play(motor: mc.Motor, play_label: tk.Label, stop_label: tk.Label) -> None:
-    motor.play()
-    play_label.config(text="再生中", bg="green")
-
-def stop(motor: mc.Motor, play_label: tk.Label, stop_label: tk.Label) -> None:
-    motor.stop()
-    play_label.config(text="停止中", bg="red")
-    
 if __name__ == "__main__":
-    main()
+    asyncio.run(main_async())
